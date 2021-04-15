3a10838dc81b575005553d770d3b0219981ce716
Kk <Kk@ZLO>
2021-04-15 16:39:11 +0300

    created new repo

---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea10 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AllowDrop = true;
+            this.label7.AutoEllipsis = true;
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Name = "label7";
+            // 
+            // chart1
+            // 
+            chartArea9.AlignmentOrientation = System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal;
+            chartArea9.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea9);
+            resources.ApplyResources(this.chart1, "chart1");
+            this.chart1.Name = "chart1";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series9.EmptyPointStyle.IsValueShownAsLabel = true;
+            series9.EmptyPointStyle.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series9.IsXValueIndexed = true;
+            series9.MarkerSize = 10;
+            series9.Name = "Series1";
+            this.chart1.Series.Add(series9);
+            // 
+            // chart2
+            // 
+            chartArea10.AlignmentOrientation = System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal;
+            chartArea10.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea10);
+            resources.ApplyResources(this.chart2, "chart2");
+            this.chart2.Name = "chart2";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series10.EmptyPointStyle.IsValueShownAsLabel = true;
+            series10.EmptyPointStyle.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series10.IsXValueIndexed = true;
+            series10.MarkerSize = 10;
+            series10.Name = "Series1";
+            this.chart2.Series.Add(series10);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        public System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TestApp/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+   
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            var header = new data.WavHeader();
+            // Размер заголовка
+            var headerSize = Marshal.SizeOf(header);
+            var fileStream = new FileStream("abc.wav", FileMode.Open, FileAccess.Read);
+            var buffer = new byte[headerSize];
+
+
+            fileStream.Read(buffer, 0, headerSize);
+            var headerPtr = Marshal.AllocHGlobal(headerSize);// Чтобы не считывать каждое значение заголовка по отдельности, воспользуемся выделением unmanaged блока памяти
+            Marshal.Copy(buffer, 0, headerPtr, headerSize); // Копируем считанные байты из файла в выделенный блок памяти
+            Marshal.PtrToStructure(headerPtr, header);  // Преобразовываем указатель на блок памяти к нашей структуре
+
+            // Посчитаем длительность воспроизведения в секундах
+            var durationSeconds = 1.0 * header.Subchunk2Size / (header.BitsPerSample / 8.0) / header.NumChannels / header.SampleRate;
+            var durationMinutes = (int)Math.Floor(durationSeconds / 60);
+            durationSeconds = durationSeconds - (durationMinutes * 60);
+
+            Int16[] Hwav = new Int16[header.ChunkSize];
+            AForge.Math.Complex[] spectr = new AForge.Math.Complex[header.ChunkSize / header.BlockAlign];
+            AForge.Math.Complex[] complexData = new AForge.Math.Complex[header.ChunkSize / header.BlockAlign];
+
+            int[,] data; // данные файла [номер канала, значение]
+
+            data = new int[header.NumChannels, header.ChunkSize / header.BlockAlign];
+            buffer = new byte[header.Subchunk2Size];
+            fileStream.Read(buffer, 0, (int)header.Subchunk2Size);
+
+            for (int i = 5; i < header.Subchunk2Size / header.BlockAlign; i++) // по количеству блоков - должно быть по семплам
+            {
+                switch (header.BlockAlign / header.NumChannels) // определяем битность - длина блока на количество каналов
+                {
+                    case 1: // 8 бит
+                        for (int y = 0; y < header.NumChannels; y++)
+                        {
+                            data[y, i] = buffer[i];
+                        }
+                        break;
+                    case 2: // 16 бит
+                        for (int y = 0; y < header.NumChannels; y++)
+                        {
+                            data[y, i] = BitConverter.ToInt16(buffer, header.BlockAlign * i);
+                        }
+                        break;
+                    case 3: // 24 бит
+                        for (int y = 0; y < header.NumChannels; y++)
+                        {
+                            data[y, i] = BitConverter.ToInt32(buffer, header.BlockAlign * i);
+                        }
+                        break;
+                    case 4: // 32 бит
+                        for (int y = 0; y < header.NumChannels; y++)
+                        {
+                            data[y, i] = BitConverter.ToInt32(buffer, header.BlockAlign * i);
+                        }
+                        break;
+                }
+
+            }
+
+            // чистим графики
+            chart1.Series[0].Points.Clear();
+            chart2.ChartAreas[0].AxisY.IsLogarithmic = true;
+            chart2.Series[0].Points.Clear();
+
+            // строим график
+            for (int i = 0; i < 1000; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, data[0,i]);
+            }
+
+            for (int i = 0; i < header.ChunkSize / header.BlockAlign; i++)
+            {
+                complexData[i] = (AForge.Math.Complex)data[0, i];
+            }
+
+
+            var dir = new AForge.Math.FourierTransform.Direction();
+            dir = (AForge.Math.FourierTransform.Direction)1;
+            AForge.Math.FourierTransform.DFT(complexData, dir);
+
+
+            for (int i = 0; i < header.ChunkSize / header.BlockAlign / 2; i++)
+            {
+                chart2.Series[0].Points.AddXY(i*header.SampleRate/(header.ChunkSize/header.BlockAlign), complexData[i].Magnitude+0.01);
+            }
+
+            //label7.Text = Convert.ToString(data[0,110012]);
+            label7.Text = Convert.ToString("SampleRate: " + header.SampleRate + " Elements: " + header.ChunkSize / header.BlockAlign);
+
+            fileStream.Close();
+
+        }
+
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            chart2.ChartAreas[0].CursorX.AutoScroll = true;
+            chart2.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            int position = 0;
+            int blockSize = 5000;
+            int size = blockSize;
+            chart2.ChartAreas[0].AxisX.ScaleView.Zoom(position, size);
+            chart2.ChartAreas[0].AxisX.ScaleView.SmallScrollSize = blockSize;
+            chart2.ChartAreas[0].AxisX.LabelStyle.Angle = 90;
+            chart2.ChartAreas[0].AxisX.LabelStyle.Interval = 150;
+        }
+    }
+
+}
